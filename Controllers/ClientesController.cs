e670f3ba2cb9b0f713525ef590788f5438c13bcf
Luiz Carlos <luizcarlossr@gmail.com>
2026-03-18 08:51:34 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,78 @@
+﻿using CadastroClientes.Models;
+using CadastroClientes.Models.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroClientes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        [HttpPost("Salvar")]
+        public object Salvar([FromBody] Clientes cadastro)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            return null;
+        }
+
+        [HttpPost("Alterar")]
+        public object Alterar([FromBody] Clientes cadastro)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            return null;
+        }
+
+        [HttpGet("Listar")]
+        public object Listar()
+        {
+            List<Clientes> listaCliente = null;
+            try
+            {
+                ClientesRepository clientesRepository = new ClientesRepository();
+                listaCliente = clientesRepository.Listar();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            return listaCliente;
+        }
+
+        [HttpDelete("Deletar")]
+        public object Deletar(int IdCliente)
+        {
+            try
+            {
+                
+                
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            return null;
+        }
+
+
+    }
+}
---
+++ Models/Repository/ClientesRepository.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace CadastroClientes.Models.Repository
+{
+    public class ClientesRepository
+    {
+        public List<Clientes> Listar() 
+        {
+            List<Clientes> clientesLista = new List<Clientes>();
+
+            //Instancia cliente Luiz Carlos
+            Clientes cliente = new Clientes();
+            cliente.UF = "SP";
+            cliente.Fax = "1111111";
+            cliente.Telefone = "2222222";
+            cliente.Documento = "333333333";
+            cliente.Email = "luiz@gmail.com";
+            cliente.Nome = "Luiz Carlos";
+            cliente.Sexo = "Masculino";
+            cliente.IdCliente = 10;
+            //Adicionando Luiz Carlos na lista de cliente
+            clientesLista.Add(cliente);
+
+            //Instancia cliente Jorge Ferreira
+            cliente = new Clientes();
+            cliente.UF = "MG";
+            cliente.Fax = "4444444";
+            cliente.Telefone = "5555555";
+            cliente.Documento = "6666666";
+            cliente.Email = "jorge@gmail.com";
+            cliente.Nome = "Jorge Ferreira";
+            cliente.Sexo = "Masculino";
+            cliente.IdCliente = 22;
+            //Adicionando Jorge Ferreira na lista de cliente
+            clientesLista.Add(cliente);
+
+            //Instancia cliente Maria da Silva
+            cliente = new Clientes();
+            cliente.UF = "RJ";
+            cliente.Fax = "7777777";
+            cliente.Telefone = "88888";
+            cliente.Documento = "999999";
+            cliente.Email = "maria@gmail.com";
+            cliente.Nome = "Maria da Silva";
+            cliente.Sexo = "Feminino";
+            cliente.IdCliente = 33;
+            //Adicionando Maria da Silva na lista de cliente
+            clientesLista.Add(cliente);
+
+            return clientesLista;
+        }
+    }
+}
